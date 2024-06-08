7056072d922228bf8775a80a1aeb517a36a10b56
aidostt <leket1999@gmail.com>
2024-06-09 03:32:17 +0500

    updated light-sail file to include instalation of git

--- lightsail-compose.sh
+++ lightsail-compose.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
-# install latest version of docker the lazy way
+# Install the latest version of Docker the lazy way
 curl -sSL https://get.docker.com | sh
 
-# make it so you don't need to sudo to run docker commands
+# Make it so you don't need to sudo to run Docker commands
 usermod -aG docker ubuntu
-
-# install docker-compose
+# Install Docker Compose
 curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
-# copy the dockerfile into /srv/docker 
-# if you change this, change the systemd service file to match
+# Install Git
+apt-get update
+apt-get install -y git
+
+# Configure Git
+git config --global core.autocrlf false
+
+# Copy the Dockerfile into /srv/docker 
+# If you change this, change the systemd service file to match
 # WorkingDirectory=[whatever you have below]
 mkdir /srv/docker
 curl -o /srv/docker/docker-compose.yml https://raw.githubusercontent.com/aidostt/diploma-backend-deploy/main/docker-compose.yml
 
-# copy in systemd unit file and register it so our compose file runs 
+# Copy in systemd unit file and register it so our compose file runs 
 # on system restart
 curl -o /etc/systemd/system/docker-compose-app.service https://raw.githubusercontent.com/aidostt/diploma-backend-deploy/main/docker-compose-app.service
 systemctl enable docker-compose-app
 
-# start up the application via docker-compose
+# Start up the application via Docker Compose
 docker-compose -f /srv/docker/docker-compose.yml up -d
 
